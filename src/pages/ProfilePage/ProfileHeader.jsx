2e7a5ca76c423f9e0df3a7b15cfe49e4c62c61d7
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2024-02-06 05:54:50 +1100

    Completed Profile Header file

---
+++ src/pages/ProfilePage/ProfileHeader.jsx
@@ -0,0 +1,87 @@
+import {
+  Avatar,
+  AvatarGroup,
+  Flex,
+  VStack,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import React from "react";
+
+const ProfileHeader = () => {
+  return (
+    <Flex
+      gap={{ base: 4, sm: 10 }}
+      py={10}
+      direction={{ base: "column", sm: "row" }}
+    >
+      <AvatarGroup
+        size={{ base: "xl", md: "2xl" }}
+        justifySelf={"center"}
+        alignSelf={"flex-start"}
+        mx={"auto"}
+      >
+        <Avatar name="honeybunnyo" src="profilepic.png" alt="Profile pic" />
+      </AvatarGroup>
+
+      <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
+        <Flex
+          gap={4}
+          direction={{ base: "column", sm: "row" }}
+          justifyContent={{ base: "center", sm: "flex-start" }}
+          alignItems={"center"}
+          w={"full"}
+        >
+          <Text fontSize={{ base: "sm", md: "lg" }}> honeybunnyo </Text>
+          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+            <Button
+              bg={"white"}
+              color={"black"}
+              _hover={{ bg: "whiteAlpha.800" }}
+              size={{ base: "xs", md: "sm" }}
+            >
+              Edit Profile
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              {" "}
+              4{" "}
+            </Text>
+            posts
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              {" "}
+              754{" "}
+            </Text>
+            followers
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              {" "}
+              673{" "}
+            </Text>
+            following
+          </Text>
+        </Flex>
+
+        <Flex alignItems={"center"} gap={4}>
+          <Text fontSize={"sm"} fontWeight={"bold"}>
+            {" "}
+            Jacqueline{" "}
+          </Text>
+        </Flex>
+
+        <Flex alignItems={"center"} gap={4}>
+          <Text fontSize={"sm"}> unsw compsci student </Text>
+        </Flex>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default ProfileHeader;
